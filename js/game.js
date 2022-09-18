7cd6d3895f58ca59ddf0edb88d7280c54ab48c5b
Chad Chapman <chad.chapman2010@gmail.com>
2022-09-18 14:00:39 -0400

    Added prompts for input for new grid.  Fixed issue where game does not work when grid is redrawn.

--- js/game.js
+++ js/game.js
@@ -9,8 +9,54 @@
  * GLOBAL VARIABLES
  *****************************************************************************/
 // Set number of columns and rows.
-const column = 16;
-const row = 16
+let column = 16;
+let row = 16
+
+
+/******************************************************************************
+ *        Name: createGrid
+ * Description: Creates the grid for Etch-a-Sketch
+ *   Arguments: column - number of columns
+ *              rows - number of rows
+ *     Returns: NONE
+ *****************************************************************************/
+function checkSize(size) {
+    if(size <= 100) {
+        column = size - 1;
+        row = size - 1;
+        createGrid(column, row);
+    } else {
+        alert("Size can't be greater than 100");
+        createGrid(16, 16);
+    }
+}
+/******************************************************************************
+ *        Name: createGrid
+ * Description: Creates the grid for Etch-a-Sketch
+ *   Arguments: column - number of columns
+ *              rows - number of rows
+ *     Returns: NONE
+ *****************************************************************************/
+ function createGrid(column, row) {
+    for(let i = 0; i < column; i++) {
+        for(j = 0; j < row; j++) {
+            const block = document.createElement('div');
+            block.classList.add('block');
+            block.style.height = container.clientHeight / column + 'px';
+            block.style.width = container.clientWidth / row + 'px';
+            block.style.backgroundColor = 'lightgrey';
+            block.setAttribute("id", `Column: ${i}, Row: ${j}`)
+            container.append(block);
+        }
+    }
+
+    // Setup event listening capability for mouse hover.
+    const blockDiv = document.querySelectorAll('.block');
+
+    blockDiv.forEach(block => block.addEventListener('mouseover', updateColor, {
+
+    }));
+}
 
 
 /******************************************************************************
@@ -26,35 +72,18 @@ function updateColor(e) {
 }
 
 
-/******************************************************************************
- *        Name: createGrid
- * Description: Creates the grid for Etch-a-Sketch
- *   Arguments: column - number of columns
- *              rows - number of rows
- *     Returns: NONE
- *****************************************************************************/
-function createGrid(column, row) {
-    for(let i = 0; i < column; i++) {
-        for(j = 0; j < row; j++) {
-            const block = document.createElement('div');
-            block.classList.add('block');
-            block.style.height = container.clientHeight / column + 'px';
-            block.style.width = container.clientWidth / row + 'px';
-            block.style.backgroundColor = 'lightgrey';
-            block.setAttribute("id", `${i},${j}`)
-            container.append(block);
-        }
-    }
-}
+
 
 // Create grid
 const container = document.querySelector('#container');
 createGrid(column, row);
 
-// Setup event listening capability for mouse hover.
-const blockDiv = document.querySelectorAll('.block');
 
-blockDiv.forEach(block => block.addEventListener('mouseover', updateColor, {
-    
-}));
 
+// Setup event listener for creating new grid size
+const button = document.querySelector('button');
+button.addEventListener('click', function() {
+    container.innerHTML = '';
+    let size = prompt("Set new size less than or equal to 100: ");
+    checkSize(size);
+});
